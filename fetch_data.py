698e2d7f0111cc159d5d3c3e047be2a9e4795178
user1177-ux <morozova.s.m.w@gmail.com>
2024-07-10 11:01:22 +0400

    Create fetch_data.py
    достаём данные из meta

---
+++ fetch_data.py
@@ -0,0 +1,33 @@
+import requests
+import csv
+
+access_token = '<YOUR_ACCESS_TOKEN>'
+ad_account_id = '<YOUR_AD_ACCOUNT_ID>'
+url = f'https://graph.facebook.com/v12.0/act_{ad_account_id}/insights'
+
+params = {
+    'fields': 'campaign_name,campaign_id,clicks,reach,impressions,actions,cpc,spend',
+    'access_token': access_token,
+    'time_increment': 1
+}
+
+response = requests.get(url, params=params)
+data = response.json()['data']
+
+with open('facebook_ads_data.csv', 'w', newline='') as csvfile:
+    fieldnames = ['Дата', 'Клики', 'Охват', 'Показы', 'Бюджет', 'Заявки', 'Кампания']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    
+    writer.writeheader()
+    for record in data:
+        lead_value = next((action['value'] for action in record['actions'] if action['action_type'] == 'lead'), 0)
+        campaign = 'RU' if 'русский' in record['campaign_name'].lower() else 'EN' if 'английский' in record['campaign_name'].lower() else 'SLO'
+        writer.writerow({
+            'Дата': record['date_start'],
+            'Клики': record['clicks'],
+            'Охват': record['reach'],
+            'Показы': record['impressions'],
+            'Бюджет': record['spend'],
+            'Заявки': lead_value,
+            'Кампания': campaign
+        })

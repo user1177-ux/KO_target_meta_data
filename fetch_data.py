df1839afce05b529c26124c00b305555a2b96778
user1177-ux <morozova.s.m.w@gmail.com>
2024-07-10 13:39:24 +0400

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -1,12 +1,11 @@
-import os
 import requests
 import csv
+import os
 
 # Получаем значения секретов из переменных окружения
 access_token = os.getenv('ACCESS_TOKEN')
 ad_account_id = os.getenv('AD_ACCOUNT_ID')
 
-# Формируем URL и параметры запроса
 url = f'https://graph.facebook.com/v12.0/act_{ad_account_id}/insights'
 params = {
     'fields': 'campaign_name,campaign_id,clicks,reach,impressions,actions,cpc,spend',
@@ -15,31 +14,31 @@ params = {
 }
 
 response = requests.get(url, params=params)
-data = response.json().get('data', [])
+response_data = response.json()
 
-# Добавим проверку, чтобы убедиться, что данные получены
+# Выводим полный ответ для диагностики
+print(f"Response: {response_data}")
+
+data = response_data.get('data')
 if not data:
     print("No data received. Check your access token and ad account ID.")
-    exit(1)
+else:
+    print(f"Data fetched: {data}")
 
-print(f"Data fetched: {data}")
-
-# Записываем данные в CSV файл
-with open('facebook_ads_data.csv', 'w', newline='') as csvfile:
-    fieldnames = ['Дата', 'Клики', 'Охват', 'Показы', 'Бюджет', 'Заявки', 'Кампания']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    for record in data:
-        lead_value = next((action['value'] for action in record.get('actions', []) if action['action_type'] == 'lead'), 0)
-        campaign = 'RU' if 'русский' in record['campaign_name'].lower() else 'EN' if 'английский' in record['campaign_name'].lower() else 'SLO'
-        writer.writerow({
-            'Дата': record['date_start'],
-            'Клики': record['clicks'],
-            'Охват': record['reach'],
-            'Показы': record['impressions'],
-            'Бюджет': record['spend'],
-            'Заявки': lead_value,
-            'Кампания': campaign
-        })
-
-print("Data written to CSV file successfully.")
+    with open('facebook_ads_data.csv', 'w', newline='') as csvfile:
+        fieldnames = ['Дата', 'Клики', 'Охват', 'Показы', 'Бюджет', 'Заявки', 'Кампания']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        
+        for record in data:
+            lead_value = next((action['value'] for action in record['actions'] if action['action_type'] == 'lead'), 0)
+            campaign = 'RU' if 'русский' in record['campaign_name'].lower() else 'EN' if 'английский' in record['campaign_name'].lower() else 'SLO'
+            writer.writerow({
+                'Дата': record['date_start'],
+                'Клики': record['clicks'],
+                'Охват': record['reach'],
+                'Показы': record['impressions'],
+                'Бюджет': record['spend'],
+                'Заявки': lead_value,
+                'Кампания': campaign
+            })

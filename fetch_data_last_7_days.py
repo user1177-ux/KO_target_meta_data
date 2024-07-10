b7e1205978db1c9b5da7101d4b1772a1db5b7ac4
user1177-ux <morozova.s.m.w@gmail.com>
2024-07-10 19:20:23 +0400

    Update fetch_data_last_7_days.py

--- fetch_data_last_7_days.py
+++ fetch_data_last_7_days.py
@@ -1,3 +1,4 @@
+# fetch_data_last_7_days.py
 import requests
 import csv
 import os
@@ -7,10 +8,10 @@ def fetch_data():
     access_token = os.getenv('ACCESS_TOKEN')
     ad_account_id = os.getenv('AD_ACCOUNT_ID')
 
+    # Даты
     end_date = datetime.now() - timedelta(days=1)
     end_date_str = end_date.strftime('%Y-%m-%d')
-    start_date = end_date - timedelta(days=7)
-    start_date_str = start_date.strftime('%Y-%m-%d')
+    start_date = (end_date - timedelta(days=7)).strftime('%Y-%m-%d')
 
     url = f'https://graph.facebook.com/v20.0/act_{ad_account_id}/campaigns'
     params = {'access_token': access_token}
@@ -33,7 +34,7 @@ def fetch_data():
         insight_params = {
             'fields': 'campaign_name,campaign_id,clicks,reach,impressions,actions,date_start,spend',
             'access_token': access_token,
-            'time_range': {'since': start_date_str, 'until': end_date_str},
+            'time_range': {'since': start_date, 'until': end_date_str},
             'time_increment': 1
         }
         response = requests.get(insight_url, params=insight_params)

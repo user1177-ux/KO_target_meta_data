c665abd4d765fed3c94a9632b772e2fa7da619a4
user1177-ux <morozova.s.m.w@gmail.com>
2024-07-10 13:28:46 +0400

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -6,6 +6,10 @@ import os
 access_token = os.getenv('ACCESS_TOKEN')
 ad_account_id = os.getenv('AD_ACCOUNT_ID')
 
+# Проверка получения токена и ID
+print("Access Token: ", access_token)
+print("Ad Account ID: ", ad_account_id)
+
 url = f'https://graph.facebook.com/v12.0/act_{ad_account_id}/insights'
 params = {
     'fields': 'campaign_name,campaign_id,clicks,reach,impressions,actions,cpc,spend',
@@ -14,19 +18,20 @@ params = {
 }
 
 response = requests.get(url, params=params)
+data = response.json()
 
-# Диагностика данных ответа
-print("Response data:")
-print(response.json())
+# Добавление отладочной информации
+print("Response data: ", data)
 
-data = response.json().get('data', [])
+data = data.get('data')
 
 if data is None:
-    print("Error: 'data' key not found in the response")
+    print("No data found in the response.")
 else:
     with open('facebook_ads_data.csv', 'w', newline='') as csvfile:
         fieldnames = ['Дата', 'Клики', 'Охват', 'Показы', 'Бюджет', 'Заявки', 'Кампания']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
         writer.writeheader()
         for record in data:
             lead_value = next((action['value'] for action in record['actions'] if action['action_type'] == 'lead'), 0)
@@ -40,3 +45,5 @@ else:
                 'Заявки': lead_value,
                 'Кампания': campaign
             })
+
+    print("Data successfully written to CSV file.")

3fd42acd1091cfd30a2dd1f3be3bb4a225d37173
user1177-ux <morozova.s.m.w@gmail.com>
2024-07-10 15:42:23 +0400

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -33,7 +33,7 @@ def fetch_data():
         insight_params = {
             'fields': 'campaign_name,campaign_id,clicks,reach,impressions,actions,date_start,spend',
             'access_token': access_token,
-            'date_preset': 'last_30d',
+            'date_preset': 'last_7d',
             'time_increment': 1
         }
         response = requests.get(insight_url, params=insight_params)

36fda624624656dd3a9ecc62b87e0acced613676
user1177-ux <morozova.s.m.w@gmail.com>
2024-07-10 13:54:28 +0400

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -1,7 +1,8 @@
-import os
 import requests
 import csv
+import os
 
+# Получаем значения секретов из переменных окружения
 access_token = os.getenv('ACCESS_TOKEN')
 ad_account_id = os.getenv('AD_ACCOUNT_ID')
 
@@ -15,25 +16,25 @@ params = {
 response = requests.get(url, params=params)
 data = response.json().get('data', [])
 
+print("Data fetched:", data)
+
 if not data:
     print("No data received. Check your access token and ad account ID.")
-    exit(1)
+else:
+    with open('facebook_ads_data.csv', 'w', newline='') as csvfile:
+        fieldnames = ['Дата', 'Клики', 'Охват', 'Показы', 'Бюджет', 'Заявки', 'Кампания']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-print(f"Data fetched: {data}")
-
-with open('facebook_ads_data.csv', 'w', newline='') as csvfile:
-    fieldnames = ['Дата', 'Клики', 'Охват', 'Показы', 'Бюджет', 'Заявки', 'Кампания']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    for record in data:
-        lead_value = next((action['value'] for action in record['actions'] if action['action_type'] == 'lead'), 0)
-        campaign = 'RU' if 'русский' in record['campaign_name'].lower() else 'EN' if 'английский' in record['campaign_name'].lower() else 'SLO'
-        writer.writerow({
-            'Дата': record['date_start'],
-            'Клики': record['clicks'],
-            'Охват': record['reach'],
-            'Показы': record['impressions'],
-            'Бюджет': record['spend'],
-            'Заявки': lead_value,
-            'Кампания': campaign
-        })
+        writer.writeheader()
+        for record in data:
+            lead_value = next((action['value'] for action in record['actions'] if action['action_type'] == 'lead'), 0)
+            campaign = 'RU' if 'русский' in record.get('campaign_name', '').lower() else 'EN' if 'английский' in record.get('campaign_name', '').lower() else 'SLO'
+            writer.writerow({
+                'Дата': record['date_start'],
+                'Клики': record['clicks'],
+                'Охват': record['reach'],
+                'Показы': record['impressions'],
+                'Бюджет': record['spend'],
+                'Заявки': lead_value,
+                'Кампания': campaign
+            })

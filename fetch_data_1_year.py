4e971b9fa77519af323acc6727bcc829f828b23c
user1177-ux <morozova.s.m.w@gmail.com>
2024-07-15 12:23:50 +0400

    Update fetch_data_1_year.py

--- fetch_data_1_year.py
+++ fetch_data_1_year.py
@@ -14,7 +14,7 @@ def fetch_data():
 
     # Даты
     end_date = datetime.now() - timedelta(days=1)
-    end_date_str = end_date.strftime('%Y-%m-%d')
+    end_date_str = '2024-07-14'
     start_date = '2024-06-01'  # Начальная дата для получения всех данных
 
     url = f'https://graph.facebook.com/v20.0/act_{ad_account_id}/campaigns'
